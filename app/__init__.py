8a172312801038d3a6dbbc9cc663e3188b1f715b
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2022-01-01 23:21:52 -0800

    Move repo

---
+++ app/__init__.py
@@ -0,0 +1,79 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+# Modules for Error Logging
+import logging
+from logging.handlers import SMTPHandler
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Note: to initiaize db for the first time, run `flask db init` at basedir
+db = SQLAlchemy(app)
+
+# To migrate, run `flask db migrate -m "<message>"`, followed by `flask db upgrade`
+# for migration update
+migrate = Migrate(app, db)
+
+# For User login (form chapter 4)
+login = LoginManager(app)
+login.login_view = "login"
+
+from app import routes, models, errors
+
+from logging.handlers import RotatingFileHandler
+import os
+
+
+# Log errors by email
+if not app.debug:
+
+    ###################################
+    # Configure Email Error Handling. #
+    ###################################
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        # Configure email handler
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure
+        )
+
+        # Set and add email handler
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    ######################################
+    # Configure logging errors to files. #
+    ######################################
+
+    # Create log dir if not exists
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    # Configure file handler
+    # Rotating File Handler ensures the log files don't grow too large (10KB limit)
+    # when the app runs for a long time.
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+
+    # Set and add file handler
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
+

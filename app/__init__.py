38eb66785a479a58972384028e9b0a892186f3fa
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2022-01-23 15:51:28 -0500

    Update init for app run

--- app/__init__.py
+++ app/__init__.py
@@ -46,6 +46,33 @@ def get_locale():
     # return "es"
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+
+################################
+# Configure logging to stdout. #
+################################
+def create_app(config_class=Config):
+    if not app.debug and not app.testing:
+        if app.config["LOG_TO_STDOUT"]:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists("logs"):
+                os.mkdir("logs")
+            file_handler = RotatingFileHandler("logs/microblog.log", maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s "
+                "[in %(pathname)s:%(lineno)d]"
+            ))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Microblog application")
+
+    return app
+
+
 #################
 # Error Logging #
 #################
@@ -96,30 +123,5 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info("Microblog startup")
+    app.logger.info("Microblog application")
 
-
-################################
-# Configure logging to stdout. #
-################################
-def create_app(config_class=Config):
-    if not app.debug and not app.testing:
-        if app.config["LOG_TO_STDOUT"]:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists("logs"):
-                os.mkdir("logs")
-            file_handler = RotatingFileHandler("logs/microblog.log", maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                "%(asctime)s %(levelname)s: %(message)s "
-                "[in %(pathname)s:%(lineno)d]"
-            ))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info("Microblog application")
-
-    return app
